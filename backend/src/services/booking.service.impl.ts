ca7b5cd46116337055253c1ff07b55a69230ae8e
Felipe Duarte dos Reis <felipe.duartedosreis@five9.com>
2023-10-15 13:57:24 +0100

    IB-47 added booking service test and implmentation skeleton

---
+++ backend/src/services/booking.service.impl.ts
@@ -0,0 +1,36 @@
+
+import { Booking } from "../models/booking.model";
+import IBookingRepository from "../repositories/booking.repository";
+import IBikeService from "./bike.service";
+import IBookingService from "./booking.service";
+
+export default class BookingService implements IBookingService {
+
+    bookingRepository: IBookingRepository;
+    bikeService: IBikeService;
+
+    constructor(bookingRepository: IBookingRepository, bikeService: IBikeService) {
+        this.bookingRepository = bookingRepository; 
+        this.bikeService = bikeService;
+    }
+
+    bookABike(userName: string, bikeSize: string): Promise<Booking> {
+        throw new Error("Method not implemented.");
+    }
+    approve(bookingId: number): Promise<Booking> {
+        throw new Error("Method not implemented.");
+    }
+    returnBike(bookingId: number): Promise<Booking> {
+        throw new Error("Method not implemented.");
+    }
+    cancel(booking: number): Promise<Booking> {
+        throw new Error("Method not implemented.");
+    }
+    listAllOpened(): Promise<Booking[]> {
+        throw new Error("Method not implemented.");
+    }
+    listInUse(): Promise<Booking[]> {
+        throw new Error("Method not implemented.");
+    }
+
+}
---
+++ backend/src/test/booking.service.test.ts
@@ -0,0 +1,8 @@
+
+import IBookingService from "../services/booking.service"
+import BookingService from "../services/booking.service.impl"
+import IBookingRepository from "../repositories/booking.repository";
+import MockBikeRepository from "./bike.fixtures";
+
+describe("For a user with no opened reservations, a new booking is created", () => {
+})
