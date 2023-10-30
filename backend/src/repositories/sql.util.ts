a023ef997b53e9f130be896593ca7d643064ac8a
Felipe Duarte <felipedreis@outlook.com>
2023-10-30 23:23:11 +0000

    IB-49 added repositories and functional tests for bike and users

---
+++ backend/src/repositories/sql.util.ts
@@ -0,0 +1,48 @@
+
+
+function isLowerCase(s: string) : boolean {
+    return "abcdefghijklmnopqrstuvwxyz".includes(s)
+}
+
+function isValid(s: string) : boolean {
+    return isLowerCase(s.toLowerCase()) || "_".includes(s)
+}
+
+function toSnakeCase(s: string) : string {
+    let result: string = ""
+    
+    for (let i = 0; i < s.length; ++i) {
+        if (!isValid(s[i]))
+            continue
+
+        if (isLowerCase(s[i]))
+            result += s[i]
+        else if("_" === s[i])
+            result += '_'
+        else 
+            result += '_' + s[i].toLocaleLowerCase()
+    }
+
+    return result
+}
+
+function createWhereClausule(searchCriteria: {}) : string {
+    let where: string = ""
+    let searchKeys = Object.keys(searchCriteria)
+
+    if (searchKeys.length > 0) {
+        where += " WHERE "
+        let whereArguments: string[] = []
+
+        searchKeys.forEach( (criteria, idx) => {
+            let sanitized = toSnakeCase(criteria)
+            whereArguments.push(`${sanitized} = $${idx + 1}`)
+        });
+        
+        where += whereArguments.reduce( (l, r) => l + " AND " + r)
+    }
+
+    return where
+}
+
+export {createWhereClausule}
