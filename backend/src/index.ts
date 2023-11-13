58cf2a75ad986a851f1035a98894f56fe7fa68bb
Felipe Duarte dos Reis <felipe.duartedosreis@five9.com>
2023-11-13 11:13:05 +0000

    added booking controllers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,46 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-//import { db } from "./db/index.js";
+import express from "express"
+import bodyParser from "body-parser"
+import cors from "cors"
+import bookingController from "./controllers/booking.controllers"
+import IBookingService from "./services/booking.service"
+import BookingService from "./services/booking.service.impl"
+import IUserRepository from "./repositories/user.repository"
+import UserRepository from "./repositories/user.repository.impl"
+import IBikeRepository from "./repositories/bike.repository"
+import BikeRepository from "./repositories/bike.repository.impl"
+import IBookingRepository from "./repositories/booking.repository"
+import BookingRepository from "./repositories/booking.repository.impl"
+import UserService from "./services/user.service.impl"
+import IBikeService from "./services/bike.service"
+import BikeService from "./services/bike.service.impl"
+import IBikeChooser from "./services/bike.chooser"
+import RandomBikeChooser from "./services/random.bike.chooser"
+import IUserService from "./services/user.service"
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+import client from "./db/index"
+
+const currentTerm = 'spring 2023'
+
+const userRepository: IUserRepository = new UserRepository(client)
+const bikeRepository: IBikeRepository = new BikeRepository(client)
+const bookingRepository: IBookingRepository = new BookingRepository(client)
+
+const userService: IUserService = new UserService(userRepository)
+const bikeService: IBikeService = new BikeService(bikeRepository)
+const bikeChooser: IBikeChooser = new RandomBikeChooser()
+const bookingService: IBookingService = new BookingService(bookingRepository, bikeService
+                                                          bikeChooser, userService, currentTerm)
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/booking/', bookingController(bookingService))
 
 app.get("/", async (req, res) => {
-  return res.status(200).send({ Response: "IPC - Alumni Bike" });
-});
+  return res.status(200).send({ Response: "IPC - Alumni Bike" })
+})
 
 app.listen(3000, () => {
-  console.log("Express server started on port 3000");
-});
+  console.log("Express server started on port 3000")
+})
