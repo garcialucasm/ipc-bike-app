53961c6fd7141fd563ead3c371f5126e5faecd92
Lucas Garcia <116840436+garcialucasm@users.noreply.github.com>
2024-09-26 00:52:59 +0100

    IB-203 add secure flag to public cookies

--- frontend/src/utils/cookies.ts
+++ frontend/src/utils/cookies.ts
@@ -2,7 +2,7 @@ export function setCookie(name: string, value: string, days: number) {
   const date = new Date()
   date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000)
   const expires = "expires=" + date.toUTCString()
-  document.cookie = `${name}=${value}; ${expires}; path=/`
+  document.cookie = `${name}=${value}; ${expires}; path=/ Secure; SameSite=None`
 }
 
 export function getCookie(name: string) {
