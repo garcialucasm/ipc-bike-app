bbb3585db592631d10bf24ef3bc263dbba2c5929
garcialucasm <garcia.lucasm@gmail.com>
2025-03-21 23:22:01 +0000

    IB-210 update title label

---
+++ frontend/src/components/Others/TitleLabel.tsx
@@ -0,0 +1,7 @@
+function TitleLabel({ children }: { children: React.ReactNode }) {
+  return <h1 className="w-full text-2xl font-extrabold">{children}</h1>
+}
+
+TitleLabel.propTypes = {}
+
+export default TitleLabel
