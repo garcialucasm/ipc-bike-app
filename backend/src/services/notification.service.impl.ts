81c3465d616f8e04da683bdd9435c8519daa704c
garcialucasm <garcia.lucasm@gmail.com>
2025-03-26 16:40:27 +0000

    IB-212 create send account approval email

--- backend/src/services/notification.service.impl.ts
+++ backend/src/services/notification.service.impl.ts
@@ -1,5 +1,6 @@
 import nodemailer from "nodemailer"
 import { getLogger } from "../logger"
+import { toTitleCase } from "../utils/strings"
 
 const logger = getLogger("Mailer")
 
@@ -33,6 +34,30 @@ export default class NotificationService {
     }
   }
 
+  static async sendAccountApprovalEmail(
+    userName: string,
+    userRole: string
+  ): Promise<void> {
+    try {
+      await NotificationService.transporter.sendMail({
+        from: process.env.GMAIL_ACCOUNT,
+        to: process.env.ADMIN_EMAIL,
+        subject: `IPC BIKE UPDATE: New Account Pending Approval`,
+        html: NotificationService.createAccountApprovalEmailHtml(
+          userName,
+          userRole
+        ),
+        headers: {
+          "List-Unsubscribe": "<mailto:ipcbikeapp@gmail.com>",
+        },
+      })
+      logger.info("Account approval email sent successfully")
+    } catch (error) {
+      logger.error("Failed to send account approval email", error)
+      throw new Error("Failed to send account approval email")
+    }
+  }
+
   private static createMaintenanceEmailHtml(numbering: number): string {
     /* ---------------------- HTML template for email body ---------------------- */
     return `
@@ -46,6 +71,7 @@ export default class NotificationService {
           max-width: 600px;
           margin: 0 auto;
           border-radius: 8px;
+          border: 1px solid #ddd;
           box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
         ">
           <h1 style="font-size: 24px; color: #2c3e50; text-align: center">
@@ -94,6 +120,90 @@ export default class NotificationService {
             message.
           </p>
       </div>
+    `
+  }
+
+  private static createAccountApprovalEmailHtml(
+    userName: string,
+    userRole: string
+  ): string {
+    const userNameFormatted = toTitleCase(userName)
+    const userRoleFormatted = toTitleCase(userRole)
+
+    /* ---------------------- HTML template for email body ---------------------- */
+    return `
+          <div
+      style="
+        font-family: Arial, sans-serif;
+        line-height: 1.6;
+        color: #333;
+        background-color: #fff;
+        padding: 20px;
+        max-width: 600px;
+        margin: 0 auto;
+        border-radius: 8px;
+        border: 1px solid #ddd;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+      ">
+      <h1 style="font-size: 24px; color: #2c3e50; text-align: center">
+        IPC Bike App
+      </h1>
+      <div style="font-size: 28px; color: #2c3e50; text-align: center">
+        👤 → 🔓
+      </div>
+      <h3 style="font-size: 16px; color: #2c3e50; text-align: center">
+        New user account requires approval
+      </h3>
+      <p style="font-size: 16px; margin-bottom: 15px">Dear Admin,</p>
+      <p style="font-size: 16px; margin-bottom: 15px">
+        A new user has registered on the IPC Bike App and is awaiting approval.
+        Please review and activate the account to allow the user to access the
+        system.
+      </p>
+      <p
+        style="
+          font-size: 16px;
+          margin-bottom: 15px;
+          padding: 15px;
+          background-color: #fce5e5;
+          border-left: 5px solid #e4980a;
+          color: #333;
+          border-radius: 5px;
+        ">
+        <strong style="color: #e4980a">Important:</strong> The user cannot log
+        in or use the system until the account has been activated by an
+        administrator. Account management can be done at
+        <strong>ipc.bike/secure/accounts</strong>
+      </p>
+      <p style="margin-bottom: 15px; line-height: 1.6;">
+        <strong style="font-size: 18px; color: #2c3e50;">Account Details:</strong>
+        <div style="font-size: 16px; margin-top: 10px; padding-left: 10px;">
+          <div style="margin-bottom: 8px;">
+            <strong style="color: #2c3e50;">Name:</strong> 
+            <span style="color: #555;">${userNameFormatted}</span>
+          </div>
+          <div>
+            <strong style="color: #2c3e50;">Role:</strong> 
+            <span style="color: #555;">${userRoleFormatted}</span>
+          </div>
+        </div>
+      </p>
+      <p style="font-size: 16px; margin-top: 30px; font-weight: bold">
+        Best regards,
+      </p>
+      <p style="font-size: 16px">IPC Bike App Support</p>
+      <p
+        style="
+          font-size: 14px;
+          color: #888;
+          text-align: center;
+          margin-top: 20px;
+          font-style: italic;
+        ">
+        Note: This is an automated notification. Please do not reply to this
+        message.
+      </p>
+    </div>
     `
   }
 }
